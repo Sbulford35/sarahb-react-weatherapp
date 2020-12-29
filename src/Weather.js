b3ec1522e698127a90d1b94d424b1e63f15f9df1
Sarah Bulford <71347490+Sbulford35@users.noreply.github.com>
2020-12-28 20:46:57 -0500

    Added local repository

---
+++ src/Weather.js
@@ -0,0 +1,117 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Duryea",
+    currently: "Sunny",
+    date: "Tue, December 8, 2020",
+    time: "5:00",
+    temperature: "63",
+    feelsLike: "44",
+    low: "36",
+    humidity: "34",
+    wind: "10",
+    sunrise: "6:48",
+    sunset: "7:02",
+    pressure: "Low",
+    img: <i className="fas fa-sun" width="100px"></i>
+  };
+
+  return (
+    <div className="Weather">
+      <p>
+        <span className="today">{weatherData.date}</span>
+        <br />
+        <span className="time">
+          <i className="far fa-clock"></i> Last Updated at {weatherData.time}pm
+        </span>
+      </p>
+      <form>
+        <div className="row">
+          <div className="col-6">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Enter city here..."
+              autocomplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <button type="submit" className="btn btn-primary w-100">
+              Search
+            </button>
+          </div>
+          <div className="col-3">
+            <button type="submit" className="btn btn-secondary w-100">
+              Current
+            </button>
+          </div>
+        </div>
+      </form>
+      <h2 className="currently">
+        <em>Your current weather in</em>
+      </h2>
+      <h1 className="searchedCity">{weatherData.city}</h1>
+      <hr />
+      <br />
+      <div className="row">
+        <div className="col-4">
+          <div className="current-sky">
+            <span className="mostly">{weatherData.currently}</span>
+            <br />
+            <span className="temperature">
+              {" "}
+              <strong>{weatherData.temperature}</strong>
+            </span>
+            <span className="units">
+              <a href="/" className="active">
+                {" "}
+                <strong>°F</strong>{" "}
+              </a>{" "}
+              |{" "}
+              <a href="/" className="active">
+                {" "}
+                <strong>°C</strong>{" "}
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="col-3">
+          <img
+            src={weatherData.img}
+            alt="weather-icon"
+            width="150px"
+            className="icon"
+          />
+        </div>
+        <div className="col-5">
+          <ul className="main-list">
+            <li className="feel">Feels like: {weatherData.feelsLike}°F</li>
+            <li className="low">Today's Low: {weatherData.low}°F</li>
+            <li className="humidity">Humidity: {weatherData.humidity}%</li>
+            <li className="wind">Wind: {weatherData.wind}mph</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-4">
+           <i className="fas fa-sunrise" width="100px"></i>
+        </div>
+        <div className="col-4">
+           <i className="fas fa-sunset" width="100px"></i>
+        </div>
+        <div className="col-4">
+          <i className="fas fa-thermometer-quarter" width="100px"></i>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4">Sunrise: {weatherData.sunrise}am</div>
+        <div className="col-4">Sunset: {weatherData.sunset}pm</div>
+        <div className="col-4">Pressure: {weatherData.pressure}</div>
+      </div>
+      <hr />
+    </div>
+  );
+}
