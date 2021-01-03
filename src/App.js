062bbc38bff9fde19f495e91fdeecea1b83e4d4b
Sarah Bulford <71347490+Sbulford35@users.noreply.github.com>
2021-01-03 17:22:45 -0500

    deleted bootstrap out of index.html and readded through react app. Also perfromed an npm update and upgrade. Moved container div to app.js from index.html.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import "./App.css";
 function App() {
     return (
     <div className="App">
+          <div class="container">
+              
         <footer><p><a href="https://github.com/Sbulford35/sarahb-react-weatherapp" target="_blank">Open source code</a> by Sarah Bulford <br />
     Hosted with <a href="https://www.netlify.com">Netlify</a></p></footer>
+    </div>
     </div>
     );
 }
