ce8dd1c7dde6551fde65a1692c02b465753723f9
Sarah Bulford <71347490+Sbulford35@users.noreply.github.com>
2021-01-03 17:43:22 -0500

    imported weather.js into app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
     return (
     <div className="App">
           <div class="container">
-              
+            <Weather />  
         <footer><p><a href="https://github.com/Sbulford35/sarahb-react-weatherapp" target="_blank">Open source code</a> by Sarah Bulford <br />
     Hosted with <a href="https://www.netlify.com">Netlify</a></p></footer>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Weather.css";
-import "./App.css";
-import App from "./App";
 
 export default function Weather() {
   let weatherData = {
