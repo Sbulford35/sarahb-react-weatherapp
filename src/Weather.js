4a1a08e62befc26d6fc39ce7d793dfd650f940f7
Sarah Bulford <71347490+Sbulford35@users.noreply.github.com>
2021-01-03 21:18:04 -0500

    console logged and set the Weather Data for City, temperature, sunrise, sunset, etc.

--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,26 @@ import "./Weather.css";
 
 export default function Weather() {
     const [ready, setReady] = useState(false);
-    const [weatherData, weatherData] = useState(null);
+    const [weatherData, setWeatherData] = useState({});
     
     
     function handleResponse(response) {
         console.log(response.data);
-        setTemperature(response.data.main.temp);
+        setWeatherData({
+        temperature: response.data.main.temp,
+        city: response.data.main.name,
+        currently: "Sunny",
+        date: "Mon, December 28, 2020",
+        time: "9:30",
+        feelsLike: response.data.main.feels_like,
+        low: response.data.main.temp_min,
+        humidity: response.data.main.humidity,
+        wind: response.data.wind.speed,
+        sunrise: response.data.sys.sunrise,
+        sunset: response.data.sys.sunset,
+        pressure: response.data.main.pressure,
+        });
+
         setReady(true);
     }
 
@@ -48,17 +62,17 @@ return (
       <h2 className="currently">
         <em>Your current weather in</em>
       </h2>
-      <h1 className="searchedCity">Duryea</h1>
+      <h1 className="searchedCity">{weatherData.city}</h1>
       <hr />
       <br />
       <div className="row">
         <div className="col-4">
           <div className="current-sky">
-            <span className="mostly">Sunny</span>
+            <span className="mostly">{weatherData.currently}</span>
             <br />
             <span className="temperature">
               {" "}
-              <strong>{Math.round(temperature)}</strong>
+              <strong>{Math.round(weatherData.temperature)}</strong>
             </span>
             <span className="units">
               <a href="/" className="active">
@@ -74,14 +88,14 @@ return (
           </div>
         </div>
         <div className="col-3">
-          <i className="fas fa-sun" width="150px"></i>
+          <i className="far fa-sun"></i>
         </div>
         <div className="col-5">
           <ul className="main-list">
-            <li className="feel">Feels like: 44°F</li>
-            <li className="low">Today's Low: 36°F</li>
-            <li className="humidity">Humidity: 34%</li>
-            <li className="wind">Wind: 10mph</li>
+            <li className="feel">Feels like: {Math.round(weatherData.feelsLike)}°F</li>
+            <li className="low">Today's Low: {Math.round(weatherData.low)}°F</li>
+            <li className="humidity">Humidity: {weatherData.humidity}%</li>
+            <li className="wind">Wind: {Math.round(weatherData.wind)} MPH</li>
           </ul>
         </div>
       </div>
@@ -98,9 +112,9 @@ return (
         </div>
       </div>
       <div className="row">
-        <div className="col-4">Sunrise: 6:48am</div>
-        <div className="col-4">Sunset: 7:02pm</div>
-        <div className="col-4">Pressure: Low</div>
+        <div className="col-4">Sunrise: {weatherData.sunrise}am</div>
+        <div className="col-4">Sunset: {weatherData.sunset}pm</div>
+        <div className="col-4">Pressure: {weatherData.pressure}</div>
       </div>
       <hr />
     </div>
@@ -108,7 +122,7 @@ return (
 }else{
     const apiKey = "738213e2d75e5700ee8029528ef19c1a";
     let city = "Duryea";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading. . .";
