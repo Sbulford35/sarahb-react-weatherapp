a4f6bc42ae8858f0439490596cfed079773810dc
Sarah Bulford <71347490+Sbulford35@users.noreply.github.com>
2021-01-06 19:47:26 -0500

    fixing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-​
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
