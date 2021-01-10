ca372a667a5ffaf4f3c9cad7a6229d2feb0ef7b4
Sarah Bulford <71347490+Sbulford35@users.noreply.github.com>
2021-01-10 14:24:01 -0500

    corrected API Key and styled forecast elements

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,30 +4,31 @@ import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-  const [loaded, setLoaded] = useState(false);
-  const [forecast, setForecast] = useState(null);
-
+const [loaded, setLoaded] = useState(false);
+const [forecast, setForecast] = useState(null);
+  
   function handleForecastResponse(response) {
     setForecast(response.data);
+    
     setLoaded(true);
   }
 
   if (loaded && props.city === forecast.city.name) {
     return (
-      <div className="WeatherForecast row">
-        <WeatherForecastPreview data={forecast.list[0]} />
-        <WeatherForecastPreview data={forecast.list[1]} />
-        <WeatherForecastPreview data={forecast.list[2]} />
-        <WeatherForecastPreview data={forecast.list[3]} />
-        <WeatherForecastPreview data={forecast.list[4]} />
-        <WeatherForecastPreview data={forecast.list[5]} />
+    <div className="WeatherForecast row">
+      <WeatherForecastPreview data={forecast.list[0]} />
+      <WeatherForecastPreview data={forecast.list[1]} />
+      <WeatherForecastPreview data={forecast.list[2]} />
+      <WeatherForecastPreview data={forecast.list[3]} />
+      <WeatherForecastPreview data={forecast.list[4]} />
+      <WeatherForecastPreview data={forecast.list[5]} />
       </div>
     );
   } else {
-    let apiKey = "5738213e2d75e5700ee8029528ef19c1a";
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=imperial`;
-    axios.get(url).then(handleForecastResponse);
-
+    let apiKey = "738213e2d75e5700ee8029528ef19c1a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleForecastResponse);
+    
     return null;
   }
 }
